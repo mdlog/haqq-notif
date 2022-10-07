3fc4867f91d29876a9a5e66524e8b9637d83c939
mdlog <adiadi2411@gmail.com>
2022-10-08 01:01:49 +0800

    Update notifhaqq.sh

--- notifhaqq.sh
+++ notifhaqq.sh
@@ -1,26 +1,27 @@
+
 #!/bin/bash
 sudo apt update && sudo apt install screen -y
 
 VALIDATOR="haqqvaloper1xmc094zr2zhkmcqw4yw6wzrrf0eaak4sgcafcr"
 ID_CHAT="485873863"
 TOKEN_BOT="5509813677:AAHUX7kAMuW0aF1Zx3NDq5ZxzUx6yJWXHZM"
-URL= https://api.telegram.org/bot$TOKEN/sendMessage
+URL="https://api.telegram.org/bot$TOKEN_BOT/sendMessage"
 while true
 do
 cd "$HOME"/ || return
 jail=$(haqqd q staking validator "$VALIDATOR" -ot | jq .jailed)
 if  [ "$jail" == "false" ]; then
-curl -s -X POST "$URL" -d "chat_id=$ID" -d "parse_mode=html" -d "text= ✅ $VALIDATOR IS FINE"
+curl -s -X POST $URL -d "chat_id=$ID_CHAT" -d "parse_mode=html" -d "text= ✅ $VALIDATOR IS FINE"
 
 elif [ "$jail" == "true" ]; then
- curl -s -X POST "$URL" -d "chat_id=$ID" -d "parse_mode=html" -d "text= ❌ $VALIDATOR IS JAILED"
+ curl -s -X POST $URL -d "chat_id=$ID_CHAT" -d "parse_mode=html" -d "text= ❌ $VALIDATOR IS JAILED"
 fi
 block=$(haqqd q slashing signing-info $(haqqd tendermint show-validator) -oj | jq .missed_blocks_counter | grep -o -E '[0-9]+')
 if  [[ "$block" -eq 0 ]]; then
-  curl -s -X POST "$URL" -d "chat_id=$ID" -d "parse_mode=html" -d "text= ✅ NODE MISSED NO BLOCK"
+  curl -s -X POST $URL -d "chat_id=$ID_CHAT" -d "parse_mode=html" -d "text= ✅ NODE MISSED NO BLOCK"
 
 elif [[ "$block" -gt 0 ]]; then
- curl -s -X POST "$URL" -d "chat_id=$ID" -d "parse_mode=html" -d "text= ⚠️ NODE IS MISSING BLOCKS: $block missed blocks"
+ curl -s -X POST $URL -d "chat_id=$ID_CHAT" -d "parse_mode=html" -d "text= ⚠️ NODE IS MISSING BLOCKS: $block missed blocks"
 fi
    printf "sleep"
         for((sec=0; sec<300; sec++))
@@ -29,7 +30,7 @@ fi
                 sleep 1
         done
         printf "\n"
-        
+
         echo "Press Ctrl+A+D to detach from this screen..."
         echo "or type: exit "
 done
