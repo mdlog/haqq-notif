68b8de75633621cd79e80074586c45a269115d8e
mdlog <adiadi2411@gmail.com>
2022-10-08 01:51:18 +0800

    Update notifhaqq.sh

--- notifhaqq.sh
+++ notifhaqq.sh
@@ -20,17 +20,17 @@ do
 cd "$HOME"/ || return
 jail=$(haqqd q staking validator "$VALIDATOR" -ot | jq .jailed)
 if  [ "$jail" == "false" ]; then
-curl -s -X POST $URL -d "chat_id=$CHAT_ID" -d "parse_mode=html" -d "text= Your ✅ $VALIDATOR IS OK"
+curl -s -X POST $URL -d "chat_id=$CHAT_ID" -d "parse_mode=html" -d "text=  ✅ Your $VALIDATOR IS OK 👍"
 
 elif [ "$jail" == "true" ]; then
- curl -s -X POST $URL -d "chat_id=$CHAT_ID" -d "parse_mode=html" -d "text= Your ❌ $VALIDATOR IS JAIL"
+ curl -s -X POST $URL -d "chat_id=$CHAT_ID" -d "parse_mode=html" -d "text= ❌ Your  $VALIDATOR IS JAIL 😒 "
 fi
 block=$(haqqd q slashing signing-info $(haqqd tendermint show-validator) -oj | jq .missed_blocks_counter | grep -o -E '[0-9]+')
 if  [[ "$block" -eq 0 ]]; then
-  curl -s -X POST $URL -d "chat_id=$CHAT_ID" -d "parse_mode=html" -d "text= Your ✅ NODE NO MISSED BLOCK"
+  curl -s -X POST $URL -d "chat_id=$CHAT_ID" -d "parse_mode=html" -d "text=  ✅ Your NODE NO MISSED BLOCK 👍"
 
 elif [[ "$block" -gt 0 ]]; then
- curl -s -X POST $URL -d "chat_id=$CHAT_ID" -d "parse_mode=html" -d "text= Your⚠️ NODE IS MISSING BLOCKS: $block missed blocks"
+ curl -s -X POST $URL -d "chat_id=$CHAT_ID" -d "parse_mode=html" -d "text= ⚠️ Your NODE IS MISSING BLOCKS: $block missed blocks 🚨"
 fi
    printf "sleep"
         for((sec=0; sec<300; sec++))
